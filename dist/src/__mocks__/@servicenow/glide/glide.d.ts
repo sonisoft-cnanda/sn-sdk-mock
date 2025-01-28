25534d8beacef40153d8a706c166002e70d5bac0
Chris Nanda <chris.nanda@tanium.com>
2025-01-28 17:30:25 -0600

    Updated

--- dist/src/__mocks__/@servicenow/glide/glide.d.ts
+++ dist/src/__mocks__/@servicenow/glide/glide.d.ts
@@ -220,4 +220,23 @@ export declare class MockGlideTime {
 }
 export declare function newMockGlideSystem(): MockGlideSystem;
 export declare const mockGs: MockGlideSystem;
+export declare class MockAbstractAjaxProcessor {
+    private CALLABLE_PREFIX;
+    private request;
+    private responseXML;
+    private gc;
+    constructor(request?: any, responseXML?: any, gc?: any);
+    process: jest.Mock<any, any, any>;
+    newItem: jest.Mock<any, any, any>;
+    getParameter: jest.Mock<any, any, any>;
+    getDocument: jest.Mock<any, any, any>;
+    getRootElement: jest.Mock<any, any, any>;
+    getName: jest.Mock<any, any, any>;
+    getValue: jest.Mock<any, any, any>;
+    getType: jest.Mock<any, any, any>;
+    getChars: jest.Mock<any, any, any>;
+    setAnswer: jest.Mock<any, any, any>;
+    setError: jest.Mock<any, any, any>;
+    type: string;
+}
 //# sourceMappingURL=glide.d.ts.map
--- dist/src/__mocks__/@servicenow/glide/glide.js
+++ dist/src/__mocks__/@servicenow/glide/glide.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.mockGs = exports.MockGlideTime = exports.MockGlideDateTime = exports.GlideDateTime = exports.GlideTime = exports.GlideDate = exports.gs = exports.MockGlideElement = exports.GlideRecord = exports.MockGlideRecord = exports.MockGlideQueryCondition = exports.MockGlideSystem = exports.MockEventQueue = exports.MockPropertyDB = exports.MockPropertyTable = exports.Database = exports.InMemoryDataTable = exports.MOCKED_PROPERTIES = void 0;
+exports.MockAbstractAjaxProcessor = exports.mockGs = exports.MockGlideTime = exports.MockGlideDateTime = exports.GlideDateTime = exports.GlideTime = exports.GlideDate = exports.gs = exports.MockGlideElement = exports.GlideRecord = exports.MockGlideRecord = exports.MockGlideQueryCondition = exports.MockGlideSystem = exports.MockEventQueue = exports.MockPropertyDB = exports.MockPropertyTable = exports.Database = exports.InMemoryDataTable = exports.MOCKED_PROPERTIES = void 0;
 exports.newMockGlideDateTime = newMockGlideDateTime;
 exports.newMockGlideSystem = newMockGlideSystem;
 const parseISO_1 = require("date-fns/parseISO");
@@ -637,4 +637,47 @@ function newMockGlideSystem() {
     return new MockGlideSystem();
 }
 exports.mockGs = new MockGlideSystem();
+class MockAbstractAjaxProcessor {
+    constructor(request, responseXML, gc) {
+        this.process = jest.fn().mockImplementation(() => {
+            return "";
+        });
+        this.newItem = jest.fn().mockImplementation((name) => {
+            return {};
+        });
+        this.getParameter = jest.fn().mockImplementation((name) => {
+            return "";
+        });
+        this.getDocument = jest.fn().mockImplementation(() => {
+            return {};
+        });
+        this.getRootElement = jest.fn().mockImplementation(() => {
+            return {};
+        });
+        this.getName = jest.fn().mockImplementation(() => {
+            return "";
+        });
+        this.getValue = jest.fn().mockImplementation(() => {
+            return "";
+        });
+        this.getType = jest.fn().mockImplementation(() => {
+            return "";
+        });
+        this.getChars = jest.fn().mockImplementation(() => {
+            return "";
+        });
+        this.setAnswer = jest.fn().mockImplementation((value) => {
+            // Do nothing
+        });
+        this.setError = jest.fn().mockImplementation((error) => {
+            // Do nothing
+        });
+        this.type = "";
+        this.CALLABLE_PREFIX = "";
+        this.request = request;
+        this.responseXML = responseXML;
+        this.gc = gc;
+    }
+}
+exports.MockAbstractAjaxProcessor = MockAbstractAjaxProcessor;
 //# sourceMappingURL=glide.js.map

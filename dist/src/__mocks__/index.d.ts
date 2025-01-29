615c60beafe57a34c486194e7fc6f28ded3e4d8e
Chris Nanda <chris.nanda@tanium.com>
2025-01-29 17:18:40 -0600

    Updated

--- dist/src/__mocks__/index.d.ts
+++ dist/src/__mocks__/index.d.ts
@@ -1,3 +1,5 @@
+export * from './MockVulnerabilityIntegrationBase.js';
+export * from './MockVulnerabilityIntegrationUtils.js';
 export * from './sn_ws_data.js';
 export * from './@servicenow/glide/index.js';
 //# sourceMappingURL=index.d.ts.map
